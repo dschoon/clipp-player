735382be2446690a884ebbfd371c6b7e35fc9d7a
dschoon <dschoon@gmail.com>
2019-03-19 16:24:33 +0000

    Updates

---
+++ precache-manifest.1931ceb4c2cba9a37196dcdd98479b4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "271d4cb6ab712fc23acc92b5e2b26298",
+    "url": "/clipp-player/static/media/playinwitme.271d4cb6.mp3"
+  },
+  {
+    "revision": "d8bfb12609b0480e49fb",
+    "url": "/clipp-player/static/js/runtime~main.71e00549.js"
+  },
+  {
+    "revision": "838cf925428ddb7604a0",
+    "url": "/clipp-player/static/js/main.24ac106b.chunk.js"
+  },
+  {
+    "revision": "fbc6f100dd308edadc70",
+    "url": "/clipp-player/static/js/2.2bc4967b.chunk.js"
+  },
+  {
+    "revision": "838cf925428ddb7604a0",
+    "url": "/clipp-player/static/css/main.73131e54.chunk.css"
+  },
+  {
+    "revision": "f7490481256d185fb2f325d420c0aabf",
+    "url": "/clipp-player/index.html"
+  }
+];
