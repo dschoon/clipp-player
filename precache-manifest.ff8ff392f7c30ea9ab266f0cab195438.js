6de7ae3676717dba76e26b563d525fdaa40d516d
dschoon <dschoon@gmail.com>
2019-12-01 17:57:14 +0000

    Updates

---
+++ precache-manifest.ff8ff392f7c30ea9ab266f0cab195438.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "373ddf9106c76910faf002a929c71eb9",
+    "url": "/clipp-player/index.html"
+  },
+  {
+    "revision": "1165da5e2db95355d164",
+    "url": "/clipp-player/static/css/main.2a6df76a.chunk.css"
+  },
+  {
+    "revision": "8aaa67198c174e3c192c",
+    "url": "/clipp-player/static/js/2.06abcc50.chunk.js"
+  },
+  {
+    "revision": "1165da5e2db95355d164",
+    "url": "/clipp-player/static/js/main.ba9dddb7.chunk.js"
+  },
+  {
+    "revision": "508193eb41fe21879c0c",
+    "url": "/clipp-player/static/js/runtime-main.cad11413.js"
+  },
+  {
+    "revision": "271d4cb6ab712fc23acc92b5e2b26298",
+    "url": "/clipp-player/static/media/playinwitme.271d4cb6.mp3"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clipp-player/precache-manifest.0b442049cb4aaea3d7ad337bc05d1d49.js"
+  "/clipp-player/precache-manifest.ff8ff392f7c30ea9ab266f0cab195438.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/clipp-player/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clipp-player/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
